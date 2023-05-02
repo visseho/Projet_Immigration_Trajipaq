cbf760637aed614613cbad5aebd267b810574c99
TheaCarlier <132284693+TheaCarlier@users.noreply.github.com>
2023-05-02 13:36:43 -0400

    Multiple changes on Bookdown

---
+++ Expérimentations (Théa)/2. Expérimentations (Bookdown)/Bookdown/src/load-packages.R
@@ -0,0 +1,13 @@
+#!/usr/bin/R
+
+library(dplyr)
+library(car)
+library(plyr)
+library(tidyverse)
+library(summarytools)
+library(readr)
+library(gplots)
+library(graphics)
+library(readxl)
+library(pander)
+library(extrafont)
